c858de28e738bb184397ec31d48f204b133b8662
silvio1501 <silvioferrentino@libero.it>
2023-11-21 18:38:43 +0100

    update contact page

--- src/pages/Contatti.jsx
+++ src/pages/Contatti.jsx
@@ -4,7 +4,7 @@ const Contatti = () => {
   return (
     <div className="w-[100%] gradiant pb-[100px]">
       <div className="w-[90%] mx-auto">
-        <div className="font-bold w-[70%] m-auto text-center md:w-[50%] py-10">
+        <div className="font-bold w-[70%] m-auto text-center md:w-[50%] pb-[80px] pt-10">
           <h1 className="text-4xl">Vorresti aggiungere un cocktail?</h1>
           <h2 className="text-white text-lg mt-5">
             Contattaci e valuteremo la tua proposta
